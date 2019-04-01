3107efe2dbe22a8514cdcd6f301c47ad47311672
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2019-04-01 18:12:31 +0500

    Implement breakpoints

--- app/debugging/bytecode_modifier.py
+++ app/debugging/bytecode_modifier.py
@@ -16,7 +16,7 @@ class BytecodeModifier:
     def modify(
             self, code: CodeType,
             breakpoints: List[int] = None,
-            *, inner=False, to_modify=False) -> CodeType:
+            *, inner=False) -> CodeType:
         initial_bytecode = Bytecode.from_code(code)
 
         modified_bytecode = Bytecode()
@@ -27,9 +27,15 @@ class BytecodeModifier:
         modified_bytecode.freevars = code.co_freevars
         modified_bytecode.cellvars = code.co_cellvars
 
-        first_breakpoint = min(breakpoints) if breakpoints else 1
-
         first_line_no = initial_bytecode.first_lineno
+
+        first_breakpoint = min(breakpoints) if breakpoints else 1
+        modified_bytecode.extend([
+            Instr('LOAD_CONST', arg=first_breakpoint, lineno=first_line_no),
+            Instr('STORE_NAME', arg='first_breakpoint', lineno=first_line_no),
+            Instr('LOAD_CONST', arg=False, lineno=first_line_no),
+            Instr('STORE_NAME', arg='is_trace', lineno=first_line_no)
+        ])
 
         if inner:
             modified_bytecode.extend([
@@ -42,7 +48,7 @@ class BytecodeModifier:
             ])
 
         # добавляем инструкции отладки перед первой строкой модуля
-        if not inner and first_breakpoint == 1:
+        if not inner:
             modified_bytecode.extend(
                 self._get_trace_func_call_instructions(first_line_no))
 
@@ -52,22 +58,14 @@ class BytecodeModifier:
                 modified_bytecode.append(instr)
                 continue
 
-            if not to_modify:
-                to_modify = instr.lineno == first_breakpoint
-
             if isinstance(instr.arg, CodeType):
                 old_instr_name = instr.name
-                bc = Bytecode.from_code(instr.arg)
-                last_lineno = bc.first_lineno + len(bc) - 1
-                to_modify_inner = not (
-                        bc.first_lineno <= first_breakpoint <= last_lineno)
                 new_co = self.modify(
-                    instr.arg, breakpoints,
-                    inner=True, to_modify=to_modify_inner)
+                    instr.arg, breakpoints, inner=True)
                 instr.set(old_instr_name, new_co)
 
             skip = Label()
-            if to_modify and instr.lineno != previous_line_no:
+            if instr.lineno != previous_line_no:
                 if inner:
                     modified_bytecode.extend([
                         Instr(
@@ -103,8 +101,20 @@ class BytecodeModifier:
         return code
 
     def _get_trace_func_call_instructions(self, line_no):
+        label = Label()
+        skip = Label()
         return [
+            Instr('LOAD_NAME', arg='is_trace', lineno=line_no),
+            Instr('POP_JUMP_IF_TRUE', arg=label, lineno=line_no),
+            Instr('LOAD_CONST', arg=line_no, lineno=line_no),
+            Instr('LOAD_NAME', arg='first_breakpoint', lineno=line_no),
+            Instr('COMPARE_OP', arg=Compare.EQ, lineno=line_no),
+            Instr('STORE_NAME', arg='is_trace', lineno=line_no),
+            label,
+            Instr('LOAD_NAME', arg='is_trace', lineno=line_no),
+            Instr('POP_JUMP_IF_FALSE', arg=skip, lineno=line_no),
             Instr('LOAD_GLOBAL', arg=self._trace_func, lineno=line_no),
             Instr('CALL_FUNCTION', arg=0, lineno=line_no),
-            Instr('POP_TOP', lineno=line_no)
+            Instr('POP_TOP', lineno=line_no),
+            skip
         ]
